2a3c8c8e5fa86ce5d05671b5efdf465196f7c188
Nyankoye <ny.loua.21@eigsi.fr>
2022-01-10 17:52:45 +0100

    ajout de sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,23 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+sentry_sdk.init(
+    dsn="https://649236292fa24bb4a5b3b876522835b6@o1112589.ingest.sentry.io/6142332",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
